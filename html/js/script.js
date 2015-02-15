322bc046b37fc405a9f3025f39d8e1c1c31fc212
Duong Linh <linhdk@dgroup.co>
2015-02-15 09:59:31 +0700

    Add slide logo

--- html/js/script.js
+++ html/js/script.js
@@ -1,57 +1,58 @@
 $(document).ready(function () {
-  var count = 0;
-  $('.main-bxslider').bxSlider();
+    $('.slide-logo').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000
+    });
+//  Remove html slick-prev, slick-next
+    $('.slick-prev').empty();
+    $('.slick-next').empty();
+//Reset tab-landing
+    $('.sub-title').click(function(){
+        $('.tab-content .tab-content .tab-pane.active').removeClass("active");
+        $('.tab-content .tab-content .tab-landing').addClass("active");
+        $('.nav-stacked li').removeClass("active");
+    })
+    var count = 0;
+    $('.main-bxslider').bxSlider();
 
-  $('.testimonials-bxSlider').bxSlider();
+    $('.testimonials-bxSlider').bxSlider();
 
-  $('#menu').slicknav({prependTo: '#main-menu'});
+    $('#menu').slicknav({prependTo: '#main-menu'});
 
-  $(window).resize(function () {
-    if ($(this).width() > 1199) {
-      $(".logo").css({"top": "0"});
-    } else {
-      if ($('.slicknav_btn').hasClass('activeMenu')) {
-        $(".logo").css({"top": "0"});
-      }
-      else {
-        $(".logo").css({"top": "-54px"});
-      }
-    }
-  });
+    $(window).resize(function () {
+        if ($(this).width() > 1199) {
+            $(".logo").css({"top": "0"});
+        } else {
+            if ($('.slicknav_btn').hasClass('activeMenu')) {
+                $(".logo").css({"top": "0"});
+            }
+            else {
+                $(".logo").css({"top": "-54px"});
+            }
+        }
+    });
 
-  $(".slicknav_btn").click(function () {
-    count++;
-    if (count % 2 != 0) {
-      $(this).addClass('activeMenu');
-      $(".slicknav_menu").css({"background-color": "#1ba2db"});
-      $(".slicknav_btn").css({"background-color": "#1ba2db"});
-      $(".logo").css({"top": "0"});
-      $(".slicknav_icon-bar").css({"background-color": "#ffffff"});
-      $(".contact-mobile .phone-mobile").hide();
-      $(".contact-mobile ul").show();
-    }
-    else {
-      $(this).removeClass('activeMenu');
-      $(".logo").css({"top": "-54px"});
-      $(".slicknav_menu").css({"background-color": "#ffffff"});
-      $(".slicknav_btn").css({"background-color": "#ffffff"});
-      $(".slicknav_icon-bar").css({"background-color": "#000000"});
-      $(".contact-mobile .phone-mobile").show();
-      $(".contact-mobile ul").hide();
-    }
-  });
-//  $('.carousel .item').each(function(){
-//    var next = $(this).next();
-//    if (!next.length) {
-//      next = $(this).siblings(':first');
-//    }
-//    next.children(':first-child').clone().appendTo($(this));
-//
-//    if (next.next().length>0) {
-//      next.next().children(':first-child').clone().appendTo($(this));
-//    }
-//    else {
-//      $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-//    }
-//  });
+    $(".slicknav_btn").click(function () {
+        count++;
+        if (count % 2 != 0) {
+            $(this).addClass('activeMenu');
+            $(".slicknav_menu").css({"background-color": "#1ba2db"});
+            $(".slicknav_btn").css({"background-color": "#1ba2db"});
+            $(".logo").css({"top": "0"});
+            $(".slicknav_icon-bar").css({"background-color": "#ffffff"});
+            $(".contact-mobile .phone-mobile").hide();
+            $(".contact-mobile ul").show();
+        }
+        else {
+            $(this).removeClass('activeMenu');
+            $(".logo").css({"top": "-54px"});
+            $(".slicknav_menu").css({"background-color": "#ffffff"});
+            $(".slicknav_btn").css({"background-color": "#ffffff"});
+            $(".slicknav_icon-bar").css({"background-color": "#000000"});
+            $(".contact-mobile .phone-mobile").show();
+            $(".contact-mobile ul").hide();
+        }
+    });
 });
